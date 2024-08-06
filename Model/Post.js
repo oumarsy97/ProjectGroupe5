e4485c82c1f9a6aa80b1c875f83174d71121ada0
rijalseydinaissa <issadiol99@gmail.com>
2024-08-06 18:28:38 +0000

    create post

---
+++ Model/Post.js
@@ -0,0 +1,69 @@
+import joi from "joi";
+import { Schema ,model} from "mongoose";
+
+const postSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+    author: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+    content:[{
+        type: String,
+        required: true,
+    }],
+    comments: [{
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+    }],
+    likes: [{
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+    }],
+    photo: String,
+    dislikes: [{
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+    }],
+    views: {
+        type: Number,
+        default: 0,
+    },
+    visibility:{
+        type: String,
+        enum: ["public", "friends"],
+        default: "public",
+    },
+    mentions:{
+        type: [{
+            type: Schema.Types.ObjectId,
+            ref: 'Tailor',
+        }],
+    },
+    
+    repost:[{
+        type: Schema.Types.ObjectId,
+        ref: 'Tailor',
+    }],
+    shares:{
+        type: [{
+            type: Schema.Types.ObjectId,
+            ref: 'User',
+        }],
+    }
+});
+
+const Post = model("Post", postSchema);
+
+export  {Post};
--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv/config';
 import connectDB from './config/database.js';
 import UserRoute from './Route/UserRoute.js';
+import PostRoute from './Route/PostRoute.js';
 connectDB();
 
 
@@ -9,6 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(`${process.env.BASE_URL}/users`, UserRoute);
+app.use(`${process.env.BASE_URL}/post`, PostRoute);
 
 
 
