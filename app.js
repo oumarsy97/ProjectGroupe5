0b8e14009f8c14a9f9a759be39706741e8b4a2af
Oumar Sy <78902232+oumarsy97@users.noreply.github.com>
2024-08-13 08:04:59 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import { config } from 'dotenv';
+import setupSwagger from './utils/swagger.js';
 import connectDB from './config/database.js';
 import UserRoute from './Route/UserRoute.js';
 import FollowRoute from './Route/FollowRoute.js';
@@ -12,13 +13,22 @@ import cron from 'node-cron';
 import { Story } from './Model/Story.js';
 import DiscussionRoute from './Route/DiscussionRoute.js';
 
+// Importation de Swagger
+import { swaggerUi, swaggerSpec } from './swaggerConfig.js';
+
+// Charger les variables d'environnement
+config();
+
 connectDB();
 
 
 
 const app = express();
 app.use(express.json());
+config();
 app.use(express.urlencoded({ extended: true }));
+
+// Routes de l'API
 app.use(`${process.env.BASE_URL}/users`, UserRoute);
 app.use(`${process.env.BASE_URL}/post`, PostRoute);
 app.use(`${process.env.BASE_URL}/follower`, FollowRoute);
@@ -26,10 +36,10 @@ app.use(`${process.env.BASE_URL}/report`, ReportRoute);
 app.use(`${process.env.BASE_URL}/discussions`, DiscussionRoute);
 app.use(`${process.env.BASE_URL}/chat`, ChatRoute);
 app.use(`${process.env.BASE_URL}/story`, StoryRoute);
-app.get('/', (req, res) => {
-    res.send('Hello World');
-});
  
+// Route pour Swagger UI
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+   
 cron.schedule('0 * * * *', async () => { 
     try {
         console.log('Deleting expired stories...'); 
@@ -40,9 +50,12 @@ cron.schedule('0 * * * *', async () => {
         console.error('Error deleting expired stories:', error);
     }
 }); 
-
  
+ // Swagger
+ setupSwagger(app);
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
+    console.log(`day mboli rek http://localhost:${port}/api-docs`);
+    
 });
